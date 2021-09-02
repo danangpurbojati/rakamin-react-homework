791c14e1e86087db4263f96f73b654a6ee7d42b1
danangpurbojati <danangpurbojati@gmail.com>
2021-09-02 08:05:04 +0700

    add initial detail-bank-page

---
+++ src/pages/DetailBank/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DetailBank = () => {
+    return (
+        <div>
+            <h1>halaman detail bank</h1>
+        </div>
+    )
+}
+
+export default DetailBank
